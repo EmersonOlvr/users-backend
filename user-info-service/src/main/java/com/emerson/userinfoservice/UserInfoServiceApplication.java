abe9d86ddfee98541b49ffdbf6bcb734cc3da5f6
EmersonOlvr <emersonolvrsouza@gmail.com>
2025-05-30 14:01:50 -0300

    Removendo dependências não usadas

--- user-info-service/src/main/java/com/emerson/userinfoservice/UserInfoServiceApplication.java
+++ user-info-service/src/main/java/com/emerson/userinfoservice/UserInfoServiceApplication.java
@@ -2,10 +2,8 @@ package com.emerson.userinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.emerson.userinfoservice.service")
 public class UserInfoServiceApplication {
 
 	public static void main(String[] args) {
